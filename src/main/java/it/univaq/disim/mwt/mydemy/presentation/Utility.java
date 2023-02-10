a25464411ef87e53230412c7ec0cf2114420a56e
fpaolant <fabiopaolantonio@gmail.com>
2023-02-10 12:16:48 +0100

    add role to logged in user and hot auth reload

--- src/main/java/it/univaq/disim/mwt/mydemy/presentation/Utility.java
+++ src/main/java/it/univaq/disim/mwt/mydemy/presentation/Utility.java
@@ -4,10 +4,15 @@ package it.univaq.disim.mwt.mydemy.presentation;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import it.univaq.disim.mwt.mydemy.domain.Utente;
 import it.univaq.disim.mwt.mydemy.security.UserDetailsImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utility {
 
@@ -20,6 +25,23 @@ public class Utility {
 			return null;
 		}
 	}
+
+	public static boolean userHasRole(String roleCode) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getAuthorities().stream()
+				.anyMatch(r -> r.getAuthority().equals(roleCode));
+	}
+
+	public static void addRole(String roleCode) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		List<GrantedAuthority> updatedAuthorities = new ArrayList<>(auth.getAuthorities());
+		updatedAuthorities.add(new SimpleGrantedAuthority(roleCode));
+
+		Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), updatedAuthorities);
+		SecurityContextHolder.getContext().setAuthentication(newAuth);
+	}
+
+
 	
 }
 
